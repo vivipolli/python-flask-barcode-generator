63e6ecad66be52ba39578003be73f333e767775e
Vivian <vivian.culn@windriver.com>
2024-02-11 18:48:23 -0300

    Feat: implement validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,14 +1,24 @@
 from src.views.http_types.http_response import HttpResponse
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "error": [{
+                    "title": error.name,
+                    "detal": error.message
+                }]
+            }
+        )
+
     return HttpResponse(
       status_code=500,
       body={
-        "error": [
-          {
+        "error": [{
             "title": "Server error",
             "detail": str(error)
-          }
-        ]
+          }]
       }
     )
