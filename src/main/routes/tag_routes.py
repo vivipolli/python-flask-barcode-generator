fb2a306bf2c76dd157ad6fb8a794141bc5ed4ba9
Vivian <vivian.culn@windriver.com>
2024-02-11 17:01:42 -0300

    Feat: add controller

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, request, jsonify
+from src.views.tag_creator_view import TagCreatorView
+from src.views.http_types.http_request import HttpRequest
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=['POST'])
 def create_tag():
-    print(request.json)
-    return jsonify({ "resp": "ok" }), 200
+    tag_creator_view = TagCreatorView()
+    http_request = HttpRequest(body=request.json)
+    response = tag_creator_view.validate_and_create(http_request)
+    return jsonify(response.body), 200
--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import CreateTagController
 
 class TagCreatorView:
     '''
@@ -7,9 +8,11 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        create_tag_controller = CreateTagController()
+
         body = http_request.body
         product_code = body["product_code"]
-        print(product_code)
 
+        formatted_response = create_tag_controller.create(product_code)
 
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
